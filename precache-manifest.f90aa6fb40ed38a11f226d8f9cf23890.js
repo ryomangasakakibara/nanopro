b817572b5bfe2963f6d153b34925ba3f74d90ec1
Ryo Sakakibara <ryomangasakakibara@gmail.com>
2022-07-19 00:32:33 +0800

    Updates

---
+++ precache-manifest.f90aa6fb40ed38a11f226d8f9cf23890.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37f57f747a5af7bfd6221b8a944190e",
+    "url": "/nanopro/index.html"
+  },
+  {
+    "revision": "d8f763ad61a4ad99b098",
+    "url": "/nanopro/static/css/main.fd994b5a.chunk.css"
+  },
+  {
+    "revision": "8286c153590e1e2fcd48",
+    "url": "/nanopro/static/js/2.c7d7de68.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/nanopro/static/js/2.c7d7de68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f763ad61a4ad99b098",
+    "url": "/nanopro/static/js/main.f48be2c3.chunk.js"
+  },
+  {
+    "revision": "2cb80c0608f9ad213a1d",
+    "url": "/nanopro/static/js/runtime-main.9af07117.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/nanopro/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/nanopro/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/nanopro/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/nanopro/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/nanopro/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/nanopro/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "bfc076e58b4cb8ae0c0af6b9b9a5a4a5",
+    "url": "/nanopro/static/media/fs_img1-02.bfc076e5.png"
+  },
+  {
+    "revision": "7d398de380c5116378a556a1a10dffd1",
+    "url": "/nanopro/static/media/fs_img2-03.7d398de3.png"
+  },
+  {
+    "revision": "876b16dfb35a50d9c1dee2ac9b3f280f",
+    "url": "/nanopro/static/media/fs_img3-04.876b16df.png"
+  },
+  {
+    "revision": "41709165ae6147569b6263ffd19758d8",
+    "url": "/nanopro/static/media/hero-02.41709165.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/nanopro/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/nanopro/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2bac68c523603cc65c0bed8167e5416e",
+    "url": "/nanopro/static/media/nanoproLogo-01.2bac68c5.svg"
+  }
+]);
